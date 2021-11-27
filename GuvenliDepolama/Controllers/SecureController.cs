02697b6ee793ab8296cebcd770c2663e6be418ef
oguzc <oguzc@DESKTOP-63TI5UI>
2021-11-27 22:26:17 +0300

    Proje dosyası ekle.

---
+++ GuvenliDepolama/Controllers/SecureController.cs
@@ -0,0 +1,32 @@
+﻿using GuvenliDepolama.Models;
+using System.Web.Mvc;
+
+namespace GuvenliDepolama.Controllers
+{
+    public class SecureController : Controller
+    {
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public string SignIn(User item)
+        {
+            return "";
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Login(User item)
+        {
+            return "";
+        }
+
+    }
+}
---
+++ GuvenliDepolama/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuvenliDepolama.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+    }
+}
